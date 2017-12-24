39a132fa48905a4dabd2b6009eafe9086aeadb23
Michelle Appel <appel.michelle@gmail.com>
2017-12-24 16:04:05 +0100

    second commit towrds cheddar

--- get_polo_data.py
+++ get_polo_data.py
@@ -2,7 +2,7 @@ import os
 import time
 import datetime
 import pandas as pd
-import csv
+import pickle
 
 CURRENCY_PAIR = 'USDT_BTC'
 START_DATE = '1388534400'	# unix timestamp for 2014.01.01
@@ -12,7 +12,7 @@ CANDLESTICK_PERIOD = '300'	# candlestick period in seconds; valid values are 300
 FETCH_URL = 'https://poloniex.com/public?command=returnChartData&currencyPair='+CURRENCY_PAIR+'&start='+START_DATE+'&period='+CANDLESTICK_PERIOD 
 
 DATA_DIR = "data"
-FILE_NAME = "data.csv"
+FILE_NAME = "data.p"
 COLUMNS = ["date","high","low","open","close","volume","quoteVolume","weightedAverage"]
 
 def timestamp_to_datetime(timestamp):
@@ -26,16 +26,17 @@ def datetime_to_timestamp(timestring): # Input string of the form '%Y-%m-%d %H:%
 		return time.mktime(datetime.datetime.strptime(timestring, '%Y-%m-%d %H:%M:%S').timetuple())	
 
 def main():
-	if not os.path.exists(DATA_DIR):
-		os.mkdir(DATA_DIR)
+	# if not os.path.exists(DATA_DIR):
+	# 	os.mkdir(DATA_DIR)
 
-	df = pd.read_json(FETCH_URL)
+	# df = pd.read_json(FETCH_URL)
 
-	df.to_csv(path_or_buf=DATA_DIR+'/'+ FILE_NAME, sep=',', na_rep='', float_format=None, 
-		columns=None, header=True, index=True, index_label=None, mode='w', 
-		encoding=None, compression=None, quoting=None, quotechar='"', 
-		line_terminator='\n', chunksize=None, tupleize_cols=None, 
-		date_format=None, doublequote=True, escapechar=None, decimal='.')
+	# df.to_pickle(path=DATA_DIR+'/'+FILE_NAME, compression='infer')
+	print('yes')
+
+	df = pickle.load(open(DATA_DIR+'/'+FILE_NAME, "rb" ))
+	print(' wat' )
+	print(df)
 
 if __name__ == '__main__':
 	main()
