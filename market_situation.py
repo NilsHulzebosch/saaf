fbf741bb7d0a59449699c4df7840365a385e619b
Michelle Appel <appel.michelle@gmail.com>
2018-01-10 10:29:58 +0100

    type error solved

--- market_situation.py
+++ market_situation.py
@@ -5,21 +5,19 @@ class Market_situation:
 		self.ticker_df = ticker_df
 		self.hist_df = hist_df
 
-		if hasattr(ticker_df, 'name'):
+		if ticker_df is not None:
 			self.name = ticker_df.name
-		else:
-			self.name = None
 
-		self.baseVolume = ticker_df['baseVolume']
-		self.high24hr = ticker_df['high24hr']
-		self.highestBid = ticker_df['highestBid']
-		self.id = ticker_df['id']
-		self.isFrozen = ticker_df['isFrozen']
-		self.last = ticker_df['last']
-		self.low24hr = ticker_df['low24hr']
-		self.lowestAsk = ticker_df['lowestAsk']
-		self.percentChange = ticker_df['percentChange']
-		self.quoteVolume = ticker_df['quoteVolume']
+			self.baseVolume = ticker_df['baseVolume']
+			self.high24hr = ticker_df['high24hr']
+			self.highestBid = ticker_df['highestBid']
+			self.id = ticker_df['id']
+			self.isFrozen = ticker_df['isFrozen']
+			self.last = ticker_df['last']
+			self.low24hr = ticker_df['low24hr']
+			self.lowestAsk = ticker_df['lowestAsk']
+			self.percentChange = ticker_df['percentChange']
+			self.quoteVolume = ticker_df['quoteVolume']
 
 	def __repr__(self):
 		return str(self.ticker_df)
